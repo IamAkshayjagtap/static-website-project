fd99c3682b284b104eaa45c471f83fac07457b3b
IamAkshayjagtap <akshayjagtap2004@gmail.com>
2025-11-29 14:44:20 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "region" {
 }
 
 variable "ami_id" {
-  default = "ami-0f5ae0fd89ce5cc6f"  # Latest Ubuntu in ap-south-1
+  default = "ami-02b8269d5e85954ef"  # Latest Ubuntu in ap-south-1
 }
 
 variable "key_name" {
